75db6f18d5318bf86d8e5379f8325f8087103f5e
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-03-04 06:06:01 -1200

    test: fix typescript errors for deploy

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,15 +1,20 @@
 import ProductPage from "@/components/selected-product/ProductPage";
 
 interface ProductProps {
-  params?: {
-    id?: string;
+  params: {
+    id: string;
   }
 }
 
 
-const Product = ({ params }:ProductProps) => {
+const Product = async ({ params }: Awaited<ProductProps>) => {
+  
+  if (!params?.id) {
+    return <div>Error: Product ID is required.</div>;
+  }
+
     return (<>
-    <ProductPage id={params?.id ?? ""} />
+    <ProductPage id={params.id} />
     </>)
 }
 
